418d0f0ce7e3ad22585c1aed8bc9d5ed80984efd
Luis Sánchez <sanchezluis9820@gmail.com>
2022-11-12 23:55:44 -0500

    removebadges popup created

---
+++ background.js
@@ -0,0 +1,19 @@
+//make extension only available at Twitter site
+const rule = {
+    conditions: [
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostSuffix: '.twitter.com', schemes: ['https'] }
+      })
+    ],
+    actions: [new chrome.declarativeContent.ShowAction()]
+  };
+
+  chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.action.disable();
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([rule]);
+    });
+  });
+
+
+
