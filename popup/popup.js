c41a09d9a7a09339bfdb8cf2e55cd89afda6021a
luiserdef <sanchezluis9820@gmail.com>
2023-03-02 00:55:20 -0500

    add change badge color option

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,60 @@
 const colorPicker = document.getElementById("color-picker")
+const blueBadgePopup = document.getElementById("blue-badge-popup")
 const badgePickColor = document.querySelectorAll(".badge-pick-color")
+const btSaveAction = document.querySelector(".bt-save-action")
+const btCancelAction = document.querySelector(".bt-cancel-action")
+const infoSaveText = document.getElementById("info-save")
+
+const LOCAL_STORAGE_NAME = "TVerifiedConfig"
+const localStorageConfig = localStorage.getItem(LOCAL_STORAGE_NAME)
+let blueVerifiedBadgeColor = '#3297c579'
+
+if(!localStorageConfig){
+    localStorageValues={
+        badgeColor:blueVerifiedBadgeColor
+    }
+    localStorage.setItem(LOCAL_STORAGE_NAME,JSON.stringify(localStorageValues))
+}else{
+    let configValues= JSON.parse(localStorageConfig)
+    blueBadgePopup.style.fill=configValues.badgeColor
+    blueVerifiedBadgeColor = configValues.badgeColor
+}
+
+//Change badge color options
+badgePickColor.forEach(element => {
+    element.style.fill= blueVerifiedBadgeColor
+});
 
 colorPicker.addEventListener("input",(e)=>{
     badgePickColor.forEach(element => {
         element.style.fill= e.target.value
     });
+    activateChangeOptions()
+    blueVerifiedBadgeColor = e.target.value
 })
+
+btSaveAction.addEventListener("click",()=>{ 
+    infoSaveText.style.display="block"
+    saveChanges("badgeColor",blueVerifiedBadgeColor)
+})
+
+function activateChangeOptions(){
+    btSaveAction.removeAttribute("disabled")
+    btSaveAction.classList.add("bt-save")
+
+    btCancelAction.removeAttribute("disabled")
+    btCancelAction.classList.add("bt-cancel")
+}
+
+
+function saveChanges(configKey,configValue){
+    let configValues= JSON.parse(localStorageConfig)
+    let newValues = {
+        ...configValues,
+        [configKey]:configValue
+    }
+    localStorage.setItem(LOCAL_STORAGE_NAME,JSON.stringify(newValues))
+
+}
 
 
