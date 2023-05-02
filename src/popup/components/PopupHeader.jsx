0f11aa6594fd0a19563fc793fab94095b6415709
luiserdef <sanchezluis9820@gmail.com>
2023-05-01 23:43:23 -0500

    Update version

--- src/popup/components/PopupHeader.jsx
+++ src/popup/components/PopupHeader.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import icon48 from '../../assets/icon48.png'
 import Github from '../assets/github.svg'
+import { CURRENT_VERSION } from '../../constants'
 
 function PopupHeader ({ txt }) {
   return (
@@ -10,7 +11,7 @@ function PopupHeader ({ txt }) {
         <h1 className='txt-title-popup'>{txt('app_title')}</h1>
       </div>
       <div className='info'>
-        <span className='ext-version'>v2.1.0 </span>
+        <span className='ext-version'>v{CURRENT_VERSION}</span>
         <a className='visit-github' href='https://github.com/luiserdef/twitter-real-verified' target='_blank' rel='noreferrer'>
           <span>GitHub</span>
           <Github width='15' height='15' />
