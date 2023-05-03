afc646cbc7575089bb53b04207c9164289500e2d
luiserdef <sanchezluis9820@gmail.com>
2023-05-03 11:26:12 -0500

    Change the data recovery of verified users

--- verifiedList/generateVerifiedUserList.py
+++ verifiedList/generateVerifiedUserList.py
@@ -1,7 +1,7 @@
 # legacy-verified.csv provided by Travisbrown:  https://gist.github.com/travisbrown/b50d6745298cccd6b1f4697e4ec22103
 
-# The outputfile is used inside "./src" file
-# This code convert legacy-verified.csv to a new file with the following struture
+# Output files is used inside "./src"
+# This code convert legacy-verified.csv to two new files with the following structure
 # var verifiedUsers= [
 #     {
 #         key:"a",
@@ -31,9 +31,21 @@ for row in data:
     else:
         users_dict[key] = {'key': key, 'users': [user]}
 
-# Convert the dictionary to a list of objects and save as JSON
-output = json.dumps(list(users_dict.values()), separators=(',', ':'))
+# Convert the dictionary to a list of objects
+users_list = list(users_dict.values())
 
-with open('verifiedUsers.jsonx', 'w') as outfile:
-    outfile.write('var verifiedUsers = ')
-    outfile.write(output)
+# Split the list in half
+half_len = len(users_list) // 2
+first_half = users_list[:half_len]
+second_half = users_list[half_len:]
+
+# Convert each half to JSON and save as separate files
+output1 = json.dumps(first_half, separators=(',', ':'))
+with open('verifiedUserList1.js', 'w') as outfile1:
+    outfile1.write('var verifiedUserList1 = ')
+    outfile1.write(output1)
+
+output2 = json.dumps(second_half, separators=(',', ':'))
+with open('verifiedUserList2.js', 'w') as outfile2:
+    outfile2.write('var verifiedUserList2 = ')
+    outfile2.write(output2)
