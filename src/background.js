f14f3702f960727b373f7cef936600193322b6b6
luiserdef <sanchezluis9820@gmail.com>
2023-05-06 02:51:06 -0500

    Fix issues with permissions in background script

--- src/background.js
+++ src/background.js
@@ -1,52 +1,79 @@
-import { validateBrowserAPI as browserAPI } from './utils/validateUserBrowser'
 
-browserAPI().tabs.onActivated.addListener(function (activeInfo) {
-  browserAPI().tabs.get(activeInfo.tabId, function (tab) {
-    updateToolbarIcon(tab.url)
-  })
-})
+const browserStatus = typeof browser !== 'undefined'
 
-browserAPI().tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  updateToolbarIcon(tab.url)
-})
-
-function updateToolbarIcon (url) {
+function updateExtensionIcon (url) {
   if (url === '') return
-  if (url.startsWith('https://twitter.com/') || url.startsWith('https://mobile.twitter.com/')) {
-    typeof browser !== 'undefined' ? browser.browserAction.enable() : chrome.action.enable()
+  if (url.startsWith('https://twitter.com/')) {
+    browser.browserAction.enable()
   } else {
-    typeof browser !== 'undefined' ? browser.browserAction.disable() : chrome.action.disable()
+    browser.browserAction.disable()
   }
 }
 
-chrome.runtime.onInstalled.addListener((details) => {
-  browserAPI().tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    updateToolbarIcon(tabs[0].url)
+function updateExtensionIconChrome () {
+  const rule1 = {
+    conditions: [
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { urlPrefix: 'https://twitter.com/' }
+      })
+    ],
+    actions: [new chrome.declarativeContent.ShowAction()]
+  }
+  chrome.action.disable()
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([rule1])
   })
-  const currentVersion = browserAPI().runtime.getManifest().version
-  const previousVersion = details.previousVersion
-  const reason = details.reason
+}
 
-  switch (reason) {
-    case 'install':
-      break
-    case 'update':
-      if (currentVersion > previousVersion) {
-        const text = { text: 'New' }
-        const color = { color: '#26ff26' }
-        if (typeof browser !== 'undefined') {
-          browser.browserAction.setBadgeText({ text: '*' })
-          browser.browserAction.setBadgeBackgroundColor(color)
-        } else {
-          chrome.action.setBadgeText(text)
-          chrome.action.setBadgeBackgroundColor(color)
+if (browserStatus) {
+  browser.tabs.onActivated.addListener(function (activeInfo) {
+    browser.tabs.get(activeInfo.tabId, function (tab) {
+      updateExtensionIcon(tab.url)
+    })
+  })
+
+  browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    updateExtensionIcon(tab.url)
+  })
+}
+
+chrome.runtime.onInstalled.addListener((details) => {
+  if (browserStatus) {
+    browser.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+      updateExtensionIcon(tabs[0].url)
+    })
+  } else {
+    updateExtensionIconChrome()
+  }
+
+  // Show a notification in the extension icon when there are UI updates within the PopUp
+  const isUIUpdate = false
+
+  if (isUIUpdate) {
+    const currentVersion = chrome.runtime.getManifest().version
+    const previousVersion = details.previousVersion
+    const reason = details.reason
+
+    switch (reason) {
+      case 'install':
+        break
+      case 'update':
+        if (currentVersion > previousVersion) {
+          const color = { color: '#26ff26' }
+          if (browserStatus) {
+            browser.browserAction.setBadgeText({ text: '*' })
+            browser.browserAction.setBadgeBackgroundColor(color)
+          } else {
+            chrome.action.setBadgeText({ text: 'New' })
+            chrome.action.setBadgeBackgroundColor(color)
+          }
         }
-      }
-      break
-    case 'chrome_update':
-    case 'shared_module_update':
-    default:
-      console.log('Other install events within the browser')
-      break
+        break
+      case 'chrome_update':
+      case 'shared_module_update':
+      default:
+        console.log('Other install events within the browser')
+        break
+    }
   }
 })
